bff9e025ae240e3a8b2b01f0339ecd5cd7ad0670
admin <admin@DESKTOP-8L2IG92>
2021-06-16 07:33:45 +0900

    ex5no10

---
+++ ex5no10/src/ex5no10/ex5no10.java
@@ -0,0 +1,34 @@
+package ex5no10;
+
+public class ex5no10 {  //  201p
+	public static void main(String[] args) {
+		int[] numArr = new int[10] ;
+		
+		for( int i = 0 ; i < numArr.length ; i++ ) {
+			System.out.print( numArr[i] = ( int )( Math.random() * 10 ));
+		}
+		System.out.println();
+		
+		for( int i = 0 ; i < numArr.length-1 ; i++ ) {
+			boolean changed = false;
+			
+			for( int j = 0 ; j < numArr.length -1 -j ; j++ ) {
+				if( numArr[j] > numArr[j+1] ) {
+					int temp = numArr[j] ;
+					numArr[j] = numArr[j+1] ;
+					numArr[j+1] = temp ;
+					changed = true ;
+				}
+			}  // for( j )
+			
+			if(!changed)break;
+			
+			for( int k = 0 ; k < numArr.length  ; k++ ) {
+				System.out.print(numArr[k]);
+			}  // for ( K )
+			System.out.println();
+		}  // for ( i )
+		
+	}
+
+}
